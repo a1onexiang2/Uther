6141cd462d2fcb59bbc9c4e7a0cbf4dfca6d02bd
NeilZheng <a1onexiang2@qq.com>
2017-08-23 11:44:46 +0800

    fix(uther): delete the redundant resources.

--- Uther/src/main/java/com/neilzheng/uther/widget/DefaultTitleBar.kt
+++ Uther/src/main/java/com/neilzheng/uther/widget/DefaultTitleBar.kt
@@ -2,9 +2,12 @@ package com.neilzheng.uther.widget;
 
 import android.app.Activity
 import android.content.Context
+import android.os.Build
+import android.support.v4.view.ViewCompat
 import android.support.v7.widget.Toolbar
 import android.util.AttributeSet
 import com.neilzheng.uther.R
+import com.neilzheng.uther.utils.dp2px
 import com.neilzheng.uther.utils.dp2pxf
 
 /**
@@ -18,7 +21,11 @@ class DefaultTitleBar : Toolbar {
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
     init {
-        elevation = dp2pxf(context, 2f)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            elevation = dp2pxf(context, 2f)
+        } else {
+            ViewCompat.setElevation(this, dp2pxf(context, 2f))
+        }
         setBackgroundColor(context.resources.getColor(R.color.colorPrimary))
         setNavigationIcon(R.drawable.abc_ic_ab_back_material)
         setNavigationOnClickListener { v ->
